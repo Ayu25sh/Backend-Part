003cd7861a2dc5afce7c5b23bdfb04b239705372
Your Name <khuranaayush08@gmail.com>
2024-04-24 20:22:59 +0530

    rename tag to section nd als added handlers

--- models/Section.js
+++ models/Section.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
 
 const sectionSchema = new mongoose.Schema({
-    name: {
+    sectionName: {
         type:String,
+        required : true,
     },
     subSection: [
         {
